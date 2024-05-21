6445f5972bdba288aae2423cc20b84ac3a2b37d6
Lab1_Kotlin <hieppvph30897@fpt.edu.vn>
2024-05-21 08:29:33 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lab4/Bai2.kt
@@ -0,0 +1,99 @@
+package com.example.lab4
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Bai2Activity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column {
+
+
+                Icon(
+                    painter = painterResource(id =
+                    R.drawable.ic_launcher_foreground),
+                    contentDescription = "Logo"
+                )
+
+                val images = listOf(R.drawable.images1, R.drawable.images2,
+                    R.drawable.images3)
+
+                HorizontalImageList(images)
+                VerticalImageList(imageList = images)
+            }
+        }
+    }
+
+    @Composable
+    fun HorizontalImageList(imageList: List<Int>) {
+        val scrollState = rememberScrollState()
+        Row(modifier = Modifier
+            .horizontalScroll(scrollState)
+            .padding(16.dp)) {
+            imageList.forEachIndexed { index, image ->
+                Image(
+                    painter = painterResource(id = image),
+                    contentDescription = "Image Description",
+                    contentScale = ContentScale.FillHeight,
+                    modifier = Modifier
+                        .size(200.dp)
+                        .clip(RoundedCornerShape(12.dp))
+                        .padding(
+                            start = if (index == 0) 0.dp else 8.dp,
+                            end = 8.dp
+                        )
+                )
+            }
+        }
+    }
+
+    @Composable
+    fun VerticalImageList(imageList: List<Int>) {
+        val scrollState = rememberScrollState()
+        Column(modifier = Modifier
+            .verticalScroll(scrollState)
+            .padding(16.dp)) {
+            imageList.forEachIndexed { index, image ->
+                Image(
+                    painter = painterResource(id = image),
+                    contentDescription = "Image Description",
+                    contentScale = ContentScale.FillWidth,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(12.dp))
+                        .padding(
+                            top = if (index == 0) 0.dp else 8.dp,
+                            bottom = 8.dp
+                        )
+                )
+            }
+        }
+    }
+}
